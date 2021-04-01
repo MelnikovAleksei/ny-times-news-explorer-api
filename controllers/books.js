5f7e62cc876e2dbe0accfe3b04fa25ee11c2c96b
Aleksei Melnikov melnkv.a.a@gmail.com <melnkv.a.a@gmail.com>
2021-04-01 10:02:35 +0300

    feat: create route /lists/overview/:date

--- controllers/books.js
+++ controllers/books.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
 
+const handleOriginalRes = require('./handleOriginalRes');
+
 const {
   NY_TIMES_API_BASE_URL,
   BOOKS_BEST_SELLERS_LISTS_URL,
@@ -10,28 +12,43 @@ const { NY_TIMES_API_KEY } = process.env;
 const getBooksBestSellersListsNames = (req, res, next) => {
     return fetch(`${NY_TIMES_API_BASE_URL}${BOOKS_BEST_SELLERS_LISTS_URL}/names.json?api-key=${NY_TIMES_API_KEY}`)
       .then((response) => {
-        const data = response.json();
-        return data;
+        return handleOriginalRes(response, res)
       })
       .then((data) => {
         res.status(200).send(data);
       })
       .catch((err) => {
+        res.send(err);
         console.log(err);
       })
       .catch(next)
 };
 
-const getBooksBestSellersListByDate = (req, res, next) => {
-  return fetch(`${NY_TIMES_API_BASE_URL}${BOOKS_BEST_SELLERS_LISTS_URL}/${req.params.date}/${req.params.list}.json?${req.params.offset}&api-key=${NY_TIMES_API_KEY}`)
+const getBooksBestSellersOverview = (req, res, next) => {
+  return fetch(`${NY_TIMES_API_BASE_URL}${BOOKS_BEST_SELLERS_LISTS_URL}/overview.json?published_date=${req.params.date}&api-key=${NY_TIMES_API_KEY}`)
     .then((response) => {
-      const data = response.json();
-      return data;
+      return handleOriginalRes(response, res)
     })
     .then((data) => {
       res.status(200).send(data);
     })
     .catch((err) => {
+      res.send(err);
+      console.log(err);
+    })
+    .catch(next)
+};
+
+const getBooksBestSellersListByDate = (req, res, next) => {
+  return fetch(`${NY_TIMES_API_BASE_URL}${BOOKS_BEST_SELLERS_LISTS_URL}/${req.params.date}/${req.params.list}.json?${req.params.offset}&api-key=${NY_TIMES_API_KEY}`)
+    .then((response) => {
+      return handleOriginalRes(response, res)
+    })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.send(err);
       console.log(err);
     })
     .catch(next)
@@ -39,5 +56,6 @@ const getBooksBestSellersListByDate = (req, res, next) => {
 
 module.exports = {
   getBooksBestSellersListsNames,
+  getBooksBestSellersOverview,
   getBooksBestSellersListByDate,
 };
--- routes/bookBestSellersList.js
+++ routes/bookBestSellersList.js
@@ -2,10 +2,12 @@ const bookBestSellersListRouter = require('express').Router();
 
 const {
   getBooksBestSellersListsNames,
+  getBooksBestSellersOverview,
   getBooksBestSellersListByDate,
 } = require('../controllers/books');
 
 bookBestSellersListRouter.get('/lists/names', getBooksBestSellersListsNames);
+bookBestSellersListRouter.get('/lists/overview/:date', getBooksBestSellersOverview);
 bookBestSellersListRouter.get('/lists/:date/:list/:offset', getBooksBestSellersListByDate);
 
 module.exports = bookBestSellersListRouter;
